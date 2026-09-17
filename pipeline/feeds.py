069903a906dabd240baa04d480740a99ac0f6556
vaanya <vaanyaagarwal.cs25@bmsce.ac.in>
2026-09-17 19:30:37 +0530

    feed list: swap Economic Times for ThePrint, add world source

---
+++ pipeline/feeds.py
@@ -0,0 +1,24 @@
+FEEDS = [
+    # National
+    {"name": "The Hindu",         "url": "https://www.thehindu.com/news/national/feeder/default.rss"},
+    {"name": "Indian Express",    "url": "https://indianexpress.com/section/india/feed/"},
+    {"name": "Hindustan Times",   "url": "https://www.hindustantimes.com/feeds/rss/india-news/rssfeed.xml"},
+    {"name": "Times of India",    "url": "https://timesofindia.indiatimes.com/rssfeeds/296589292.cms"},
+    {"name": "NDTV",              "url": "https://feeds.feedburner.com/ndtvnews-india-news"},
+    {"name": "Business Standard", "url": "https://www.business-standard.com/rss/home_page_top_stories.rss"},
+    {"name": "The Print",         "url": "https://theprint.in/feed/"},
+    {"name": "Mint",              "url": "https://www.livemint.com/rss/news"},
+    # Karnataka
+    {"name": "Deccan Herald",     "url": "https://www.deccanherald.com/rss-feeds/state.rss"},
+    {"name": "The Hindu",         "url": "https://www.thehindu.com/news/national/karnataka/feeder/default.rss"},
+    {"name": "Times of India",    "url": "https://timesofindia.indiatimes.com/rssfeeds/1981579271.cms"},
+    # Uttar Pradesh
+    {"name": "Hindustan Times",   "url": "https://www.hindustantimes.com/feeds/rss/lucknow/rssfeed.xml"},
+    {"name": "Times of India",    "url": "https://timesofindia.indiatimes.com/rssfeeds/1370153311.cms"},
+    {"name": "Indian Express",    "url": "https://indianexpress.com/section/cities/lucknow/feed/"},
+    # International
+    {"name": "Reuters",           "url": "https://news.google.com/rss/search?q=site:reuters.com&hl=en-IN&gl=IN&ceid=IN:en"},
+    {"name": "AP",                "url": "https://news.google.com/rss/search?q=site:apnews.com&hl=en-IN&gl=IN&ceid=IN:en"},
+    {"name": "BBC News",          "url": "https://feeds.bbci.co.uk/news/world/rss.xml"},
+    {"name": "Al Jazeera",        "url": "https://www.aljazeera.com/xml/rss/all.xml"},
+]
