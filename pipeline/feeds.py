802728cfd48b3cf9b2682327cc0111a49dcca277
vaanya <vaanyaagarwal.cs25@bmsce.ac.in>
2026-09-17 19:36:46 +0530

    working feed list and feed test

--- pipeline/feeds.py
+++ pipeline/feeds.py
@@ -7,10 +7,15 @@ FEEDS = [
     {"name": "NDTV",              "url": "https://feeds.feedburner.com/ndtvnews-india-news"},
     {"name": "Business Standard", "url": "https://www.business-standard.com/rss/home_page_top_stories.rss"},
     {"name": "Mint",              "url": "https://www.livemint.com/rss/news"},
+    {"name": "The Print",         "url": "https://news.google.com/rss/search?q=site:theprint.in+when:2d&hl=en-IN&gl=IN&ceid=IN:en"},
     # Karnataka
     {"name": "The Hindu",         "url": "https://www.thehindu.com/news/national/karnataka/feeder/default.rss"},
+    {"name": "Deccan Herald",     "url": "https://news.google.com/rss/search?q=site:deccanherald.com+when:2d&hl=en-IN&gl=IN&ceid=IN:en"},
+    {"name": "Times of India",    "url": "https://news.google.com/rss/search?q=site:timesofindia.indiatimes.com+Karnataka+when:2d&hl=en-IN&gl=IN&ceid=IN:en"},
     # Uttar Pradesh
     {"name": "Indian Express",    "url": "https://indianexpress.com/section/cities/lucknow/feed/"},
+    {"name": "Hindustan Times",   "url": "https://news.google.com/rss/search?q=site:hindustantimes.com+Lucknow+when:2d&hl=en-IN&gl=IN&ceid=IN:en"},
+    {"name": "Times of India",    "url": "https://news.google.com/rss/search?q=site:timesofindia.indiatimes.com+Lucknow+when:2d&hl=en-IN&gl=IN&ceid=IN:en"},
     # International
     {"name": "Reuters",           "url": "https://news.google.com/rss/search?q=site:reuters.com&hl=en-IN&gl=IN&ceid=IN:en"},
     {"name": "AP",                "url": "https://news.google.com/rss/search?q=site:apnews.com&hl=en-IN&gl=IN&ceid=IN:en"},
