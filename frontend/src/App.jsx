a83a9d843f42e8b537ba12dd9df84b2fa19318fd
Hamsini <hamsini@Hamsinis-MacBook-Air.local>
2026-09-19 08:29:48 +0530

    app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-semibold">POV</h1>
+    </div>
+  )
+}
+
+export default App
